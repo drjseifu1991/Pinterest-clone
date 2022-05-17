ded5181c1b6ffbc9ac0614047e61884b814c823e
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-05-17 15:15:12 +0300

    My second commit

---
+++ src/components/TopNav.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="px-3 py-4 flex items-center justify-between w-full">
+        <div class="flex items-center p-0">
+            <button class="rounded-full hover:bg-light p-3 flex items-center justify-center" type="button">
+                <img src="../assets/pinterest-logo.png" alt="Logo" class="h-9 w-9">
+            </button>
+            <button v-for="item in tabItems" :key="item.key" @click="selectedTab = item.key" :class="`${selectedTab === item.key ? 'bg-dark text-white' : 'text-dark hover:bg-light'}`" class="rounded-full px-4 py-3 font-semibold">
+                {{ item.name }}
+            </button>
+        </div>
+        <div class="flex-grow relative ml-2 mr-2">
+            <span class="material-icons absolute mt-3 ml-3 text-muted">search</span>
+            <input class="w-full hover:bg-lightHover rounded-full bg-light py-3 pr-3 pl-10" placeholder="Search" name="" value="" type="text">
+        </div>
+        <div class="flex items-center">
+            <button class="relative rouded-full hover:bg-light flex items-center justify-center p-3">
+                <span class="material-icons text-3xl text-muted">notifications</span>
+                <div class="rounded-full h-5 w-5 bg-primary text-white flex items-center justify-center absolute ml-5 mb-5">4</div>
+            </button>
+            <button class="relative rouded-full hover:bg-light flex items-center justify-center p-3">
+                <span class="material-icons text-3xl text-muted">textsms</span>
+                <div class="rounded-full h-5 w-5 bg-primary text-white flex items-center justify-center absolute ml-5 mb-5">1</div>
+            </button>
+            <button class="rounded-full hover:bg-light p-3 flex items-center justify-center" type="button">
+                <img  src="../assets/profile.jpg" alt="Logo" class="h-9 w-9 rounded-full">
+            </button>
+
+            <button class="hover:bg-light w-6 h-6 flex items-center justify-center rounded-full mr-2" type="button">
+                <span class="material-icons text-muted">expand_more</span>
+            </button>
+        </div>
+    </div>
+</template>>
+
+<script>
+export default {
+    data() {
+        return {
+            tabItems: [
+                {name: 'Home', key: 'home'},
+                {name: 'Today', key: 'today'}
+            ],
+            selectedTab: 'home'
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#E60023',
+        'dark': '#111111',
+        'light': '#EFEFEF',
+        'lightHover': '#D7D7D7',
+        'muted': '#717171'
+      }
+    },
   },
   plugins: [],
 }
