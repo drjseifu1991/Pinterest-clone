69e6353ce7d24002b3639d3db5db71cf61eb4cfc
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-05-18 20:51:44 +0300

    After adding card.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
   import TopNav from './components/TopNav.vue';
+  import CardItem from './components/CardItem.vue';
   export default {
     name: 'App',
     components: {
-      TopNav
+      TopNav, CardItem
+    },
+    data() {
+      image: './assets/profile.jpg'
     }
   }
 </script>
 
 <template>
-  <top-nav></top-nav>
+  <top-nav/>
+  <div class="px-20 ">
+    <div class="w-1/5">
+      <card-item v-bind:src="image" class="w-full h-full"/>
+    </div>
+  </div>
 </template>
 
 <style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div @mouseover="hover = true" @mouseleave="hover = false" class="relative">
+        <img v-bind:src="src" alt="no image" class="rounded-lg">
+        <div v-if="hover" class="absolute bg-dark opacity-50 inset-8 w-full rounded-lg"></div>
+        <div v-if="hover" class="absolute top-0 w-full flex flex-col justify-between p-3">
+            <div class="flex items-center justify-between">
+                <button type="button" name="button" class="flex item-center">
+                    <p class="font-semibold text-white">UI</p>
+                    <span class="material-icons text-white">expand_more</span>
+                </button>
+                <button class="rounded-full py-3 px-5 text-white bg-primary font-semibold" type="button" name="button">
+                    Save
+                </button>
+            </div>
+            <div class="flex items-center w-full">
+                <button class="ml-auto mr-2 bg-white rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100" name="button" type="button">
+                    <span class="material-icons text-base">ios_share</span>
+                </button>
+                <button class="bg-white rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100" name="button" type="button">
+                    <span class="material-icons text-base">more_horiz</span>
+                </button>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['src'],
+        data() {
+            return {
+                hover: false
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
